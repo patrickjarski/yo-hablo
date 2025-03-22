daef62d5acaac9ad0dea4870b4bab3024e0259e3
Patryk Jarski <pjarski@interia.pl>
2025-03-22 17:24:50 +0000

    Imported data and visualized infinitive verbs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
+import {StrictMode, useEffect, useState} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import verbData from "./data/jehle_verb_lookup.json";
+
+enum TENSES {
+  PRESENT,
+  IMPERFECT,
+  FUTURE
+}
+
+type VerbObject = {
+  performer: string, // add persons type
+  mood: string,
+  infinitive: string,
+  performer_en: string, // add persons type
+  tense: string,
+  has_long: boolean,
+  translation: string
+}
+
+type VerbDataEntry = [string, Array<VerbObject>];
+
+const Main = () => {
+  const [pickedTense, setPickedTense] = useState(TENSES.PRESENT);
+  const [verbsForPickedTense, setVerbsForPickedTense] = useState([]);
+  console.log(Object.fromEntries(Object.entries(verbData).slice(0,10)));
+
+  useEffect(() => {
+    const filteredVerbs = Object.entries(verbData)
+      .filter((verbEntry: VerbDataEntry) =>
+        verbEntry[1][0].tense.toLowerCase() === TENSES[pickedTense].toLowerCase())
+      .map((verbEntry: VerbDataEntry) => verbEntry[1][0].infinitive)
+      .filter((value: string, index: number, array: Array<string>) => array.indexOf(value) === index)
+      .sort();
+
+    setVerbsForPickedTense(filteredVerbs);
+  }, [pickedTense])
+
+  return verbsForPickedTense.map(verb => <p>{verb}</p>);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
