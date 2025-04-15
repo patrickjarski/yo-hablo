0051d168a4013f29cd580db692b6d446f465d86a
Patryk Jarski <pjarski@interia.pl>
2025-04-15 08:03:37 +0200

    Added basic conjugate feature

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,15 @@ import { VERBS_ES } from "./data";
 import { Dashboard } from "./pages/dashboard";
 import { FillInTheBlank } from "./pages/fill-in-the-blank";
 import { Flashcards } from "./pages/flashcards";
+import { Conjugate } from "./pages/conjugate";
 
-import './index.css'
+import './index.css';
 
 const router = createBrowserRouter([
   { path: "dashboard", Component: Dashboard, loader: () => ({ verbs: VERBS_ES }) },
   { path: "flashcards", Component: Flashcards, loader: () => ({ verbs: VERBS_ES }) },
   { path: "fill-in-the-blank", loader: () => ({ verbs: VERBS_ES }), Component: FillInTheBlank },
-  { path: "conjugate", loader: () => ({ verbs: VERBS_ES }), Component: Flashcards },
+  { path: "conjugate", loader: () => ({ verbs: VERBS_ES }), Component: Conjugate },
 ])
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/conjugate/Conjugate.tsx
+++ src/pages/conjugate/Conjugate.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Verb } from "../../data";
+import React, { useCallback, useEffect, useState } from "react";
+import { TENSES, Verb } from "../../data";
 import { useLoaderData } from "react-router";
 
 type ConjugateLoaderData = {
@@ -9,7 +9,61 @@ type ConjugateLoaderData = {
 const Conjugate = () => {
   const { verbs } = useLoaderData<ConjugateLoaderData>();
 
-  console.log(verbs);
+  const [filteredVerbs, setFilteredVerbs] = useState<Verb[]>([]);
+  const [currentVerbIndex, setCurrentVerbIndex] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
 
-  return "CONJUGATE";
+  const resetForm = useCallback(() => {
+    setInputValue("");
+    setIsCorrect(false);
+  }, [])
+
+  useEffect(() => {
+    const filtered = verbs.filter(verb => Object.keys(verb.conjugations).includes("PRESENT"));
+
+    setFilteredVerbs(filtered);
+  }, [verbs])
+
+  useEffect(() => {
+    if (filteredVerbs.length && inputValue === filteredVerbs[currentVerbIndex].conjugations["PRESENT"][0].conjugatedVerb) {
+      setIsCorrect(true);
+    }
+  }, [inputValue])
+
+  if (!filteredVerbs.length) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h3><b>{filteredVerbs[currentVerbIndex].infinitive}</b></h3>
+      <p>
+        {filteredVerbs[currentVerbIndex].conjugations["PRESENT"][0].performer}
+        <input disabled={isCorrect} value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+      </p>
+
+      <br/>
+      <button
+        onClick={() => {
+          if (currentVerbIndex + 1 < verbs.length) {
+            setCurrentVerbIndex(prevIndex => prevIndex + 1);
+            resetForm();
+          }
+        }}
+      >NEXT VERB
+      </button>
+      <button
+        onClick={() => {
+          if (currentVerbIndex > 0) {
+            setCurrentVerbIndex(prevIndex => prevIndex - 1)
+            resetForm();
+          }
+        }}
+      >PREV VERB
+      </button>
+    </div>
+  );
 }
+
+export { Conjugate };
